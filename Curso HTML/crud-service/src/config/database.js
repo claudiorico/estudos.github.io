78ef0af13c09062dfecbf8532acca6c18e5a9e34
claudiorico <claudiorico81@hotmail.com>
2023-01-31 23:49:30 -0300

    30/01/2023

---
+++ Curso HTML/crud-service/src/config/database.js
@@ -0,0 +1,26 @@
+/**
+ * Arquivo: config/database.js
+ * Descrição: arquivo responsável pelas 'connectionStrings da aplicação: PostgreSQL.
+ * Data: 04/03/2020
+ * Author: Glaucia Lemos
+ */
+
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+console.log(process.env) // remove this after you've confirmed it is working
+
+dotenv.config();
+
+// ==> Conexão com a Base de Dados:
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+  console.log('Base de Dados conectado com sucesso!');
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
